5d0c801d173ddc74634c72266aac2fa5c92d40fd
Vadim Dunaevskiy <vadim.dunaevskiy23@imperial.ac.uk>
2023-10-16 18:35:25 +0100

    I have added a page that sums two integers that the user inputs in a second form

--- api/app.py
+++ api/app.py
@@ -11,3 +11,11 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+@app.route("/submit_sum", methods=["POST"])
+def sumbit_sum():
+    input_num_1 = request.form.get("num_1")
+    input_num_2 = request.form.get("num_2")
+    return render_template("sum.html", num1=input_num_1, num2=input_num_2)
+
+
