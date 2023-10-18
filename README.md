a22ead54a536294f4eed0b50a2c830b653074246
Mariia Shapovalova <ms7023@ic.ac.uk>
2023-10-18 19:37:55 +0100

    adding atm functionality to the website

---
+++ README.md
@@ -0,0 +1,21 @@
+hello-world-website/
+│
+├── api/                         
+│   ├── app.py          
+│   ├── static/         
+│   │   ├── styles.css
+│   │   └── images/
+│   │       └── ATM.jpeg
+│   │
+│   └── templates/         # HTML templates
+│       ├── atm.html
+│       ├── errors.html     
+│       ├── hello.html     
+│       ├── index.html    
+│       ├── money.html 
+│       ├── result.html 
+│       └── sum.html 
+│
+├── requirements.txt       # Python package dependencies
+│
+└── vercel.json            # Configuration for Vercel deployment
--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -13,9 +13,22 @@ def submit():
     return render_template("hello.html", name=input_name, age=input_age)
 
 @app.route("/submit_sum", methods=["POST"])
-def sumbit_sum():
+def submit_sum():
     input_num_1 = request.form.get("num_1")
     input_num_2 = request.form.get("num_2")
     return render_template("sum.html", num1=input_num_1, num2=input_num_2)
+
+@app.route("/atm", methods=["GET", "POST"])
+def atm():
+    return render_template("atm.html")
+
+@app.route("/atm/money", methods=["POST"])
+def withdraw_money():
+    dollar_amount = int(request.form.get("dollarAmount", 0))
+    # check if the dollar amount is within the valid range (1 to 50)
+    if 1 <= dollar_amount <= 50:
+        return render_template("money.html", dollars=dollar_amount)
+    else:
+        return render_template("errors.html", dollars=dollar_amount)
 
 
